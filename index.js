29f4726958fcbc0268c1a04cc2e786243617c80e
Roseline001 <roseline.jonathan@student.moringaschool.com>
2026-09-23 11:06:53 +0300

    functions introduction

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+function calculateTax(amount) {
+    let taxValue = amount * 0.1;
+    return taxValue;
+}
 
+function convertToUpperCase(text) {
+    let upperCaseText = text.toUpperCase()
+    return upperCaseText;
+}
 
+function findMaximum(num1, num2) {
+    return Math.max(num1, num2);
+}
+
+function isPalindrome(word) {
+    let originalWord = word.toLowerCase();
+    let reversedWord = originalWord.split('').reverse().join('');
+    return originalWord === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    let discountAmount = originalPrice * (discountPercentage / 100);
+    let expectedDiscountedPrice = originalPrice - discountAmount;
+    return expectedDiscountedPrice;
+}
 
 
 
